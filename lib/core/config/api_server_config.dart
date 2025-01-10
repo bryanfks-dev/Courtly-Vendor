75f55191c4d0ff34786fc0e3be8074d4915a2c1d
bryanfks-dev <bryanfernandoks11@gmail.com>
2025-01-10 15:51:13 +0700

    comment api_server_config

--- lib/core/config/api_server_config.dart
+++ lib/core/config/api_server_config.dart
@@ -4,5 +4,6 @@ class ApiServerConfig {
   static const String baseUrl = 'http://192.168.1.77:3000';
 
   /// [version] is the version of the API server.
+  /// [version] should always start with 'v' followed by the version number.
   static const String version = 'v1';
 }
